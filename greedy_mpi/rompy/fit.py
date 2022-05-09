5889a0b51a38f1a8a019cf6705f93e4713aa27a9
Ror Smith <rory.smith@ligo.org>
2022-05-08 21:37:04 -0700

    readded rompy

---
+++ greedy_mpi/rompy/fit.py
@@ -0,0 +1,288 @@
+# --- fit.py ---
+
+"""
+	Classes and functions for fitting functions at the greedy-selected parameter values.
+"""
+
+
+import numpy as np
+from scipy.interpolate import splrep
+from scipy.interpolate import splev
+#from scipy.optimize import curve_fit
+from scipy.optimize import leastsq
+import scipy.optimize as opt
+import eim, lib
+
+
+##############################################
+# The following three functions (_general_function, _weighted_general_function, 
+# curve_fit) are from scipy.optimize. I have changed the error handling here
+# so that curve_fit returns the parameters found, even if they are not optimal. 
+# The original code raises an exception and aborts the program. Using a `try` and `except`
+# pair does not seem to retain the (suboptimal) fit parameters.
+
+def _general_function(params, xdata, ydata, function):
+    return function(xdata, *params) - ydata
+
+
+def _weighted_general_function(params, xdata, ydata, function, weights):
+    return weights * (function(xdata, *params) - ydata)
+
+
+def curve_fit(f, xdata, ydata, p0=None, sigma=None, **kw):
+    """
+    Use non-linear least squares to fit a function, f, to data.
+
+    Assumes ``ydata = f(xdata, *params) + eps``
+
+    Parameters
+    ----------
+    f : callable
+        The model function, f(x, ...).  It must take the independent
+        variable as the first argument and the parameters to fit as
+        separate remaining arguments.
+    xdata : An N-length sequence or an (k,N)-shaped array
+        for functions with k predictors.
+        The independent variable where the data is measured.
+    ydata : N-length sequence
+        The dependent data --- nominally f(xdata, ...)
+    p0 : None, scalar, or M-length sequence
+        Initial guess for the parameters.  If None, then the initial
+        values will all be 1 (if the number of parameters for the function
+        can be determined using introspection, otherwise a ValueError
+        is raised).
+    sigma : None or N-length sequence
+        If not None, this vector will be used as relative weights in the
+        least-squares problem.
+
+    Returns
+    -------
+    popt : array
+        Optimal values for the parameters so that the sum of the squared error
+        of ``f(xdata, *popt) - ydata`` is minimized
+    pcov : 2d array
+        The estimated covariance of popt.  The diagonals provide the variance
+        of the parameter estimate.
+
+    See Also
+    --------
+    leastsq
+
+    Notes
+    -----
+    The algorithm uses the Levenberg-Marquardt algorithm through `leastsq`.
+    Additional keyword arguments are passed directly to that algorithm.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from scipy.optimize import curve_fit
+    >>> def func(x, a, b, c):
+    ...     return a*np.exp(-b*x) + c
+
+    >>> x = np.linspace(0,4,50)
+    >>> y = func(x, 2.5, 1.3, 0.5)
+    >>> yn = y + 0.2*np.random.normal(size=len(x))
+
+    >>> popt, pcov = curve_fit(func, x, yn)
+
+    """
+    if p0 is None:
+        # determine number of parameters by inspecting the function
+        import inspect
+        args, varargs, varkw, defaults = inspect.getargspec(f)
+        if len(args) < 2:
+            msg = "Unable to determine number of fit parameters."
+            raise ValueError(msg)
+        if 'self' in args:
+            p0 = [1.0] * (len(args)-2)
+        else:
+            p0 = [1.0] * (len(args)-1)
+
+    if np.isscalar(p0):
+        p0 = array([p0])
+
+    args = (xdata, ydata, f)
+    if sigma is None:
+        func = _general_function
+    else:
+        func = _weighted_general_function
+        args += (1.0/np.asarray(sigma),)
+
+    # Remove full_output from kw, otherwise we're passing it in twice.
+    return_full = kw.pop('full_output', False)
+    res = leastsq(func, p0, args=args, full_output=1, **kw)
+    (popt, pcov, infodict, errmsg, ier) = res
+
+    if ier not in [1, 2, 3, 4]:
+        msg = "Optimal parameters not found: " + errmsg
+        #raise RuntimeError(msg)
+        print (msg)
+        return popt, pcov
+        #if return_full:
+        #    return popt, pcov, infodict, errmsg, ier
+        #else:
+        #    return popt, pcov 
+
+    if (len(ydata) > len(p0)) and pcov is not None:
+        s_sq = (func(popt, *args)**2).sum()/(len(ydata)-len(p0))
+        pcov = pcov * s_sq
+    else:
+        pcov = np.inf
+
+    if return_full:
+        return popt, pcov, infodict, errmsg, ier
+    else:
+        return popt, pcov
+
+
+
+##############################################
+class Spline1d:
+	"""Class for building one dimensional spline interpolants"""
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __init__(self, x, y, k=3):
+		"""Initialize Spline1d class. Builds 1d spline interpolant 
+		of degree k from x and y data.
+		
+		Input
+		-----
+		   x   -- given array of parameter values
+		   y   -- function values to be interpolated (e.g., at empirical
+		   		   interpolation nodes 
+		   k    -- degree of spline (default is 3)
+		"""
+		
+		args = np.argsort(x)	# `x` values must be sorted in ascending order
+		self.dim = np.shape(y)
+		self.degree = k
+		if len(self.dim) == 1:
+			self.knots, self.coeffs, _ = splrep(x[args], y[args], k=k)
+		elif len(self.dim) > 1:
+			self.coeffs = []
+			for yy in y:
+				temp = splrep(x[args], yy[args], k=k)
+				self.coeffs.append(temp[1])
+				self.knots = temp[0]
+		else:
+			raise Exception( "Expected a non-empty array.")
+		
+		self.fitparams = [self.knots, self.coeffs, self.degree]
+		
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __call__(self, p, der=0):
+		return self.fit(p, der)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def fit(self, p, der=0):
+		if len(self.dim) == 1:
+			return splev(p, self.fitparams, der)
+		elif len(self.dim) > 1:
+			return np.array([splev(p, [self.knots, self.coeffs[ii], self.degree]) for ii in range(self.dim[0])])
+	
+
+##############################################
+class Polynomial:
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __init__(self, x, y, deg=3):
+		
+		# Map x to reference interval, [-1,1]
+		ref = lib.map_intervals(x, -1., 1.)
+		
+		self.shape = np.shape(y)
+		if len(self.shape) == 1:
+			self.fitparams_ref = np.polyfit(ref, y, deg=deg)
+			self.fitparams = self.map_poly_coeffs(self.fitparams_ref, ref, np.min(x), np.max(x))
+		elif len(self.shape) > 1:
+			if len(np.shape(deg)) == 0:
+				self.fitparams_ref = [np.polyfit(ref, y[ii], deg=deg) for ii in range(self.shape[0])]
+			else:
+				self.fitparams_ref = [np.polyfit(ref, y[ii], deg=deg[ii]) for ii in range(self.shape[0])]
+			self.fitparams = [self.map_poly_coeffs(pp, ref, np.min(x), np.max(x)) for pp in self.fitparams_ref]
+		else:
+			raise Exception( "Expected a non-empty array.")
+		pass
+
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def map_poly_coeffs(self, coeffs, x, a, b):
+		"""Map polynomial coefficients from p(x) to that on the interval [a,b]"""
+		
+		# Linear transformation from x to [a,b]
+		M = (np.max(x)-np.min(x))/(b-a)
+		B = np.min(x)-M*a
+		
+		# Map the polynomial coefficients
+		deg, mapped_coeffs = len(coeffs), []
+		c = coeffs[::-1]	# Reverse order of polynomial coefficients
+		for n in range(deg):
+			mapped_coeffs.append( M**n*np.sum(lib.choose(kk,n)*c[kk]*B**(kk-n) for kk in range(n,deg)) )
+		
+		return mapped_coeffs[::-1]	# Reverse order of polynomial coefficients for use with np.polyval
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __call__(self, p):
+		return self.fit(p)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def fit(self, p):
+		if len(self.shape) == 1:
+			return np.polyval(self.fitparams, p)
+		elif len(self.shape) > 1:
+			return np.array([np.polyval(self.fitparams[ii], p) for ii in range(self.shape[0])])
+	
+
+##############################################
+class UserDefined:
+
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __init__(self, x, y, fn, p0=None, maxfev=1000):
+		self.shape = y.shape
+		self.fn = fn
+		if len(self.shape) == 1:
+			self.fitparams = curve_fit(fn, x, y, p0=p0, maxfev=maxfev)[0]
+		elif len(self.shape) > 1:
+			self.fitparams = [curve_fit(fn, x, y[ii], p0=p0, maxfev=maxfev)[0] for ii in range(self.shape[0])]
+		else:
+			raise Exception( "Expected a non-empty array.")
+		pass
+
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __call__(self, p):
+		return self.fit(p)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def fit(self, p):
+		if len(self.shape) == 1:
+			return self.fn(p, *self.fitparams)
+		elif len(self.shape) > 1:
+			return np.array([self.fn(p, *self.fitparams[ii]) for ii in range(self.shape[0])])
+	
+
+##############################################
+#class Surrogate(Spline1d, Polynomial, UserDefined, eim.EmpiricalInterpolation):
+class Surrogate(Spline1d, Polynomial, UserDefined):
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __init__(self, args, fit_type):
+		#eim.EmpiricalInterpolation.__init__(self)
+		
+		if fit_type == 'spline-1d':
+			Spline1d.__init__(self, *args)
+			self.fit = Spline1d(*args).fit
+		elif fit_type == 'poly':
+			Polynomial.__init__(self, *args)
+			self.fit = Polynomial(*args).fit
+		elif fit_type == 'userdef':
+			UserDefined.__init__(self, *args)
+			self.fit = UserDefined(*args).fit
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __call__(self, p, B):
+		return eval(p, B)
+
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def eval(self, p, B):
+		return np.dot(self.fit(p), B)
+	
---
+++ greedy_mpi/rompy/integrals.py
@@ -0,0 +1,450 @@
+# --- integrals.py ---
+
+"""    
+    Classes and functions for computing inner products of functions
+"""
+
+import numpy as np
+from training import TrainingTools
+
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def rate_to_num(a, b, rate):
+	"""Convert sample rate to sample numbers in [a,b]"""
+	return np.floor(np.float(b-a)*rate)+1
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def num_to_rate(a, b, num):
+	"""Convert sample numbers in [a,b] to sample rate"""
+	return (num-1.)/np.float(b-a)
+
+	
+##############################################
+class Quadratures(TrainingTools):
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __init__(self):
+		TrainingTools.__init__(self)
+		self.switch_dict = {
+			'riemann-rate': self.riemann_rate,
+			'riemann-num': self.riemann_num,
+			'trapezoidal-rate': self.trapezoidal_rate,
+			'trapezoidal-num': self.trapezoidal_num,
+			'chebyshev': self.chebyshev,
+			'chebyshev-gauss-lobatto': self.chebyshev_gauss_lobatto,
+			'legendre': self.legendre,
+			'legendre-gauss-lobatto': self.legendre_gauss_lobatto,
+			#'roq': self.roq
+			}
+		self.options = self.switch_dict.keys()
+		
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __call__(self, args, quad_type):#, multidomainQ=False):
+		
+		dim = np.shape(args[:-1])
+		num_or_rate = args[-1]
+		
+		# Check that the number of domains equals to the number of samples/sample rates specified
+		if type(num_or_rate) == list:
+			if len(num_or_rate) != dim[0]-1:
+				raise Exception( "Expecting equal number of intervals and number of samples/sample rates.")
+		
+		# Check how many quadrature rules are specified
+		if type(quad_type) == list:
+			type_quad_type = 'list'
+			if len(quad_type) != len(num_or_rate):
+				raise Exception( "Expecting equal number of intervals and specified quadrature rules.")
+		else:
+			type_quad_type = 'str'
+		
+		# Make quadrature rules for single or multiple domains
+		if len(dim) == 1:
+			
+			# Single integration domain
+			if dim[0] == 2:
+				ans =  self.switch_dict[quad_type](*args)
+			
+			# Multiple integration domains
+			if dim[0] > 2:
+				if type_quad_type == 'str':
+					ans = np.hstack([self.switch_dict[quad_type](args[ii], args[ii+1], num_or_rate[ii]) for ii in range(dim[0]-1)])
+				elif type_quad_type == 'list':
+					ans = np.hstack([self.switch_dict[quad_type[ii]](args[ii], args[ii+1], num_or_rate[ii]) for ii in range(dim[0]-1)])
+					
+		elif len(dim) > 1:
+			# CHECKME
+			# For integrating in multiple dimensions
+			nodes, weights = [], []
+			for ii in range(len(args[0])):
+				arg = np.transpose(args)[ii]
+				quad = quad_type[ii]
+				nws = self.switch_dict[quad](*arg)
+				nodes.append(nws[0])
+				weights.append(nws[1])
+			weights_nd = np.transpose(self.tuple_to_vstack(self.meshgrid2(*weights)))
+			ans = [self.tuple_to_vstack(self.meshgrid2(*nodes)).T, np.array([np.prod(weights_nd[ii]) for ii in range(len(weights_nd[:,0]))])]
+		
+		return ans
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def riemann_num(self, a, b, n):
+		"""
+		Uniformly sampled array using Riemann quadrature rule  
+		over given interval with given number of samples
+		
+		Input
+		-----
+		a -- start of interval
+		b -- end of interval
+		n -- number of quadrature points
+		
+		Output
+		------
+		nodes   -- quadrature nodes
+		weights -- quadrature weights
+		"""
+		nodes = np.linspace( a, b, num=n )
+		weights = np.ones(n, dtype='double')
+		return [nodes, (b-a)/(n-1.)*weights]
+
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def riemann_rate(self, a, b, rate):
+		"""
+		Uniformly sampled array using Riemann quadrature rule  
+		over given interval with given sample rate
+		
+		Input
+		-----
+		a    -- start of interval
+		b    -- end of interval
+		rate -- sample rate
+		
+		Output
+		------
+		nodes   -- quadrature nodes
+		weights -- quadrature weights
+		"""
+		n = rate_to_num(a, b, rate)
+		return self.riemann_num(a, b, n)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def trapezoidal_num(self, a, b, n):
+		"""
+		Uniformly sampled array using trapezoidal quadrature rule  
+		over given interval with given number of samples
+		
+		Input
+		-----
+		a -- start of interval
+		b -- end of interval
+		n -- number of quadrature samples
+		
+		Output
+		------
+		nodes   -- quadrature nodes
+		weights -- quadrature weights
+		"""
+		nodes = np.linspace(a, b, num=n)
+		weights = np.ones(n, dtype='double')
+		weights[0] /= 2.
+		weights[-1] /= 2.
+		return [nodes, weights*(b-a)/(n-1.)]
+
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def trapezoidal_rate(self, a, b, rate):
+		"""
+		Uniformly sampled array using trapezoidal quadrature rule  
+		over given interval with given sample rate
+		
+		Input
+		-----
+		a    -- start of interval
+		b    -- end of interval
+		rate -- sample rate
+		
+		Output
+		------
+		nodes   -- quadrature nodes
+		weights -- quadrature weights
+		"""
+		n = rate_to_num(a, b, rate)
+		return self.trapezoidal_num(a, b, n)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def chebyshev(self, a, b, n):
+		"""
+		Uniformly sampled array using Chebyshev-Gauss quadrature rule  
+		over given interval with given number of samples
+		
+		Input
+		-----
+		a -- start of interval
+		b -- end of interval
+		n -- number of quadrature samples
+		
+		Output
+		------
+		nodes   -- quadrature nodes
+		weights -- quadrature weights
+		"""
+		num = int(n)-1.
+		nodes = np.array([-np.cos(np.pi*(2.*ii+1.)/(2.*num+2.)) for ii in range(int(n))])
+		weights = np.pi/(num+1.) * np.sqrt(1.-nodes**2)
+		return [nodes*(b-a)/2.+(b+a)/2., weights*(b-a)/2.]
+		
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def chebyshev_gauss_lobatto(self, a, b, n):
+		"""
+		Uniformly sampled array using Chebyshev-Gauss-Lobatto quadrature rule  
+		over given interval with given number of samples
+		
+		Input
+		-----
+		a -- start of interval
+		b -- end of interval
+		n -- number of quadrature samples
+		
+		Output
+		------
+		nodes   -- quadrature nodes
+		weights -- quadrature weights
+		"""
+		num = int(n)-1.
+		nodes = np.array([-np.cos(np.pi*ii/num) for ii in range(int(n))])
+		weights = np.pi/num * np.sqrt(1.-nodes**2.)
+		weights[0] /= 2. 
+		weights[-1] /= 2.
+		return [nodes*(b-a)/2.+(b+a)/2., weights*(b-a)/2.]
+		
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def legendre(self, a, b, num):
+		raise Exception( "Legendre Gauss quadrature rule is not yet implemented.")
+		pass
+
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def legendre_gauss_lobatto(self, a, b, num):
+		raise Exception( "Legendre Gauss-Lobatto quadrature rule is not yet implemented.")
+		pass
+
+#	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#	def roq(self, x, w, eim_indices, B):
+#		"""Reduced-order quadrature rule
+#		
+#		Input
+#		-----
+#		x 			-- original quadrature nodes (e.g., Chebyshev)
+#		w 			-- original quadrature weights
+#		eim_indices	-- array indices of empirical interpolation nodes
+#		B 			-- empirical interpolant operator, `B`
+#		
+#		Returns
+#		-------
+#		nodes 	-- reduced-order quadrature nodes
+#		weights -- reduced-order quadrature weights
+#		
+#		Examples
+#		--------
+#		Let x, w be 20 Chebyshev Gauss nodes and weights over the interval 
+#		[10, 100] from
+#		
+#		  >>> qu = rp.Quadratures()
+#		  
+#		  >>> x, w = qu([10, 100, 20], 'chebyshev')
+#		
+#		If `B` is the empirical interpolant operator so that the empirical
+#		interpolant acting on a function `h` is I[h] = \sum_{i=1}^m B_i*h_i
+#		and `inds` are the array indices associated with the empirical 
+#		interpolation nodes then
+#		
+#		  >>> roq_x, roq_w = rp.Quadratures()(x, w, inds, B)
+#		
+#		gives the reduced-order quadrature nodes (`roq_x`) and weights 
+#		(`roq_w`).
+#		
+#		"""
+#		
+#		nodes = x[eim_indices]
+#		weights = np.dot(B, w)
+#		return [nodes, weights]
+		
+
+##############################################
+class Real:
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __init__(self, args, quad_type='trapezoidal-num'):
+		self.quad_type = quad_type
+		self.nodes, self.weights = Quadratures()(args, quad_type)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def integral(self, h):
+		"""Integral of a function"""
+		return np.dot(self.weights, np.real(h))
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def dot(self, h1, h2):
+		"""Dot product of two functions"""
+		dim = np.shape(h1)
+		h1real = np.real(h1)
+		h2real = np.real(h2)
+		if len(dim) == 1:
+			return np.dot(self.weights, h1real*h2real)
+		else:
+			return np.dot(self.weights, np.sum( h1real[ii]*h2real[ii] for ii in range(dim[0])))
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def norm(self, h):
+		"""Norm of function"""
+		hreal = np.real(h)
+		return np.sqrt(np.real(self.dot(hreal, hreal)))
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def normalize(self, h):
+		"""Normalize a function"""
+		return h / self.norm(h)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def match(self, h1, h2):
+		"""Match integral"""
+		h1n = self.normalize(h1)
+		h2n = self.normalize(h2)
+		return np.real(self.dot(h1n, h2n))
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def mismatch(self, h1, h2):
+		"""Mismatch integral (1-match)"""
+		return 1.-self.match(h1, h2)
+
+
+##############################################
+class Complex:
+
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __init__(self, args, quad_type='trapezoidal-num'):
+		self.quad_type = quad_type
+		self.nodes, self.weights = Quadratures()(args, quad_type)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def integral(self, h):
+		"""Integral of a function"""
+		return np.dot(self.weights, h)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def dot(self, h1, h2):
+		"""Dot product of two functions"""
+		dim = np.shape(h1)
+		if len(dim) == 1:
+			return self.integral(np.conj(h1)*h2)
+		else:
+			return self.integral(np.sum(np.conj(h1[ii])*h2[ii] for ii in range(dim[0])))
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def norm(self, h):
+		"""Norm of function"""
+		return np.sqrt(np.real(self.dot(h, h)))
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def normalize(self, h):
+		"""Normalize a function"""
+		return h / self.norm(h)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def match(self, h1, h2):
+		"""Match integral"""
+		h1n = self.normalize(h1)
+		h2n = self.normalize(h2)
+		return np.real(self.dot(h1n, h2n))
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def mismatch(self, h1, h2):
+		"""Mismatch integral (1-match)"""
+		return 1.-self.match(h1, h2)
+
+
+##############################################
+class InnerProduct(Real, Complex):
+	"""Integrals for computing inner products and norms of functions"""
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __init__(self, args, quad_type='trapezoidal-num'):
+		
+		Real.__init__(self, args, quad_type)
+		self.real = Real(args, quad_type)
+		
+		Complex.__init__(self, args, quad_type)
+		self.complex = Complex(args, quad_type)
+		
+		self.switch_dict = {
+			'complex': self.complex,
+			'real': self.real,
+			}
+		self.options = self.switch_dict.keys()
+		
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def __call__(self, h1, h2, inner_type):
+		return self.dot(h1, h2, inner_type)
+			
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def integral(self, h, inner_type):
+		"""Integral of a function"""
+		return self.switch_dict[inner_type].integral(h)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def dot(self, h1, h2, inner_type):
+		"""Dot product of two functions"""
+		return self.switch_dict[inner_type].dot(h1, h2)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def norm(self, h, inner_type):
+		"""Norm of function"""
+		return np.sqrt(np.real(self.dot(h, h, inner_type)))
+		
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def normalize(self, h, inner_type):
+		"""Normalize a function"""
+		return h / self.norm(h, inner_type)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def match(self, h1, h2, inner_type):
+		"""Match integral"""
+		return self.switch_dict[inner_type].match(h1, h2)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def mismatch(self, h1, h2, inner_type):
+		"""Mismatch integral (1-match)"""
+		return self.switch_dict[inner_type].mismatch(h1, h2)
+		
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def Linfty(self, h):
+		"""L-infinity norm"""
+		# FIXME: h cannot be a multi-dim array
+		return np.max(np.abs(h))
+		
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def Ln(self, h, n):
+		"""L-n norm"""
+		# FIXME: h cannot be a multi-dim array
+		if n > 0:
+			return (np.dot(self.weights, np.abs(h)**n))**(1./n)
+		pass
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def L2(self, h):
+		"""L-2 norm"""
+		# FIXME: h cannot be a multi-dim array
+		return self.Ln(h, 2)
+	
+	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	def _test_monomial(self, n=0):
+		"""Test integration rule by integrating the monomial x**n"""
+		ans = self.integral(self.nodes**n, 'real')
+		a, b = self.nodes[0], self.nodes[-1]
+		expd = (b**(n+1)-a**(n+1))/(n+1)
+		
+		print( "\nExpected value for integral =", expd)
+		print( "Computed value for integral =", ans)
+		print( "\nAbsolute difference =", expd-ans)
+		print( "Relative difference =", 1.-ans/expd)
+		
+		pass
+
