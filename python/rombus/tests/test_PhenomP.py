d3324e244043404592cf83ad7a1375c12d0cd6a0
Gregory Poole <gbpoole@gmail.com>
2022-12-23 20:29:58 +1100

    Fix PhenomP unit test to work with the new runtime declaration of model code.

--- python/rombus/tests/test_PhenomP.py
+++ python/rombus/tests/test_PhenomP.py
@@ -3,18 +3,18 @@ from rombus.scripts.cli import cli
 from click.testing import CliRunner
 import numpy as np
 
-def test_lal(tmp_path):
+def test_PhenomP(tmp_path):
 
     atol = 1e-12
     rtol = 1e-12
 
     runner = CliRunner()    
     with runner.isolated_filesystem(temp_dir=tmp_path):
-        result = runner.invoke(cli,["make-reduced-basis", "/Users/gpoole/my_code/rombus/python/tests/data/LALSuite_test_grid.npy"])
+        result = runner.invoke(cli,["rombus.tests.PhenomP:model","make-reduced-basis", "/Users/gpoole/my_code/rombus/python/rombus/tests/data/LALSuite_test_grid.npy"])
         assert result.exit_code == 0
-        result = runner.invoke(cli,["make-empirical-interpolant"])
+        result = runner.invoke(cli,["rombus.tests.PhenomP:model","make-empirical-interpolant"])
         assert result.exit_code == 0
-        result = runner.invoke(cli,["compare-rom-to-true"])
+        result = runner.invoke(cli,["rombus.tests.PhenomP:model","compare-rom-to-true"])
         assert result.exit_code == 0
         delta_diff = np.load("ROM_diff.npy")
 
