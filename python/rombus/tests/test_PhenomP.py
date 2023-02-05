8680d62049b9c17828860e841b6407b3465a6ced
Gregory Poole <gbpoole@gmail.com>
2023-02-05 20:29:30 +1100

    Fix broken LALSuite test.

--- python/rombus/tests/test_PhenomP.py
+++ python/rombus/tests/test_PhenomP.py
@@ -26,7 +26,18 @@ def test_PhenomP(tmp_path):
         )
         assert result.exit_code == 0
         result = runner.invoke(
-            cli, ["rombus.tests.PhenomP:model", "compare-rom-to-true"]
+            cli,
+            [
+                "rombus.tests.PhenomP:model",
+                "compare-rom-to-true",
+                "m1=23.07487209351506",
+                "m2=29.753779794217984",
+                "chi1L=0.3803919479347709",
+                "chi2L=0.266269755913278",
+                "chip=0.5458029422691579",
+                "thetaJ=1.7621026471916568",
+                "alpha=2.765791991075064",
+            ],
         )
         assert result.exit_code == 0
         delta_diff = np.load("ROM_diff.npy")
