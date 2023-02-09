87553079e44623f3baf002cbe1291915aad9839a
Gregory Poole <gbpoole@gmail.com>
2023-02-09 23:43:05 +1100

    Fix a broken import in a test.

--- python/rombus/tests/test_rombus.py
+++ python/rombus/tests/test_rombus.py
@@ -1,6 +1,6 @@
-import rombus
+import rombus.integrals as integrals
 
 
 def test_rate_to_num():
-    x = rombus.integrals.rate_to_num(0, 10, 50)
+    x = integrals.rate_to_num(0, 10, 50)
     assert x is not None
