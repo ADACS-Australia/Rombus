83231adbc3192649257882c53d996a51f96917b4
Gregory Poole <gbpoole@gmail.com>
2023-03-13 21:27:54 +1100

    Fix ordering of the commands displayed with '--help'

--- python/rombus/cli.py
+++ python/rombus/cli.py
@@ -2,6 +2,8 @@ import sys
 
 import click
 
+import collections
+
 import rombus.core as core
 import rombus.plot as plot
 
@@ -15,7 +17,20 @@ FLEX_CONTEXT_SETTINGS = dict(
 )
 
 
-@click.group(context_settings=CONTEXT_SETTINGS)
+class _OrderedGroup(click.Group):
+    """This class is used for to ensure that the ordering of the CLI subcommands
+    are in code-order in the CLI help and documentation."""
+
+    def __init__(self, name=None, commands=None, **attrs):
+        super(_OrderedGroup, self).__init__(name, commands, **attrs)
+        #: the registered subcommands by their exported names.
+        self.commands = commands or collections.OrderedDict()
+
+    def list_commands(self, ctx):
+        return self.commands
+
+
+@click.group(cls=_OrderedGroup, context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 def cli(ctx):
     """Perform greedy algorythm operations with Rombus"""
@@ -23,25 +38,6 @@ def cli(ctx):
     # ensure that ctx.obj exists and is a dict (in case `cli()` is called
     # by means other than the `if` block below)
     ctx.ensure_object(dict)
-
-
-@cli.command(context_settings=FLEX_CONTEXT_SETTINGS)
-@click.argument("filename_ROM", type=str)
-@click.argument("parameters", nargs=-1, type=click.UNPROCESSED)
-@click.pass_context
-def evaluate(ctx, filename_rom, parameters):
-    """Evaluate a reduced order model and compare it to truth
-
-    PARAMETERS is a list of parameter values of the form A=VAL B=VAL ..."""
-
-    # Read ROM
-    ROM = core.ROM.from_file(filename_rom)
-
-    # Parse the model parameters, which should have been given as arguments
-    model_params = ROM.model.parse_cli_params(parameters)
-
-    # Generate plot
-    plot.compare_rom_to_true(ROM, model_params)
 
 
 @cli.command(context_settings=CONTEXT_SETTINGS)
@@ -97,6 +93,25 @@ def refine(ctx, filename_rom):
     filename_split = filename_rom.rsplit(".", 1)
     filename_out = f"{filename_split[0]}_refined.{filename_split[1]}"
     ROM.write(filename_out)
+
+
+@cli.command(context_settings=FLEX_CONTEXT_SETTINGS)
+@click.argument("filename_ROM", type=str)
+@click.argument("parameters", nargs=-1, type=click.UNPROCESSED)
+@click.pass_context
+def evaluate(ctx, filename_rom, parameters):
+    """Evaluate a reduced order model and compare it to truth
+
+    PARAMETERS is a list of parameter values of the form A=VAL B=VAL ..."""
+
+    # Read ROM
+    ROM = core.ROM.from_file(filename_rom)
+
+    # Parse the model parameters, which should have been given as arguments
+    model_params = ROM.model.parse_cli_params(parameters)
+
+    # Generate plot
+    plot.compare_rom_to_true(ROM, model_params)
 
 
 if __name__ == "__main__":
