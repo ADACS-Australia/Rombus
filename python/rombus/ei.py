77d548afa1c4b24e4c4ef2efe7614f99db5fe899
Gregory Poole <gbpoole@gmail.com>
2023-03-28 10:47:26 +1100

    Remove a whole bunch of legacy code and move a bunch of EI code from rombus._core.eim.py to ei.py.

--- python/rombus/ei.py
+++ python/rombus/ei.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+import time
 from typing import Self
 
-import rombus._core.algorithms as algorithms
+from scipy.linalg.lapack import get_lapack_funcs  # type: ignore
+
 import rombus._core.mpi as mpi
 import rombus._core.hdf5 as hdf5
 import rombus.exceptions as exceptions
@@ -11,6 +13,247 @@ from rombus.reduced_basis import ReducedBasis
 
 DEFAULT_TOLERANCE = 1e-14
 DEFAULT_REFINE_N_RANDOM = 100
+
+
+def malloc(dtype, *nums):
+    """Allocate some memory with given dtype"""
+    return np.zeros(tuple(nums), dtype=dtype)
+
+
+class LinAlg:
+    """Linear algebra functions needed for empirical interpolation class"""
+
+    def __init__(self):
+        pass
+
+    # This is scipy.linalg's source code. For some reason my scipy doesn't recognize
+    # the check_finite option, which may help with speeding up. Because this may be an
+    # issue related to the latest scipy.linalg version, I'm reproducing that code here
+    # to guarantee future compatibility.
+    def solve_triangular(
+        self,
+        a,
+        b,
+        trans=0,
+        lower=False,
+        unit_diagonal=False,
+        overwrite_b=False,
+        debug=False,
+        check_finite=True,
+    ):
+        """
+        Solve the equation `a x = b` for `x`, assuming a is a triangular matrix.
+
+        Parameters
+        ----------
+        a : (M, M) array_like
+            A triangular matrix
+        b : (M,) or (M, N) array_like
+            Right-hand side matrix in `a x = b`
+        lower : boolean
+            Use only data contained in the lower triangle of `a`.
+            Default is to use upper triangle.
+        trans : {0, 1, 2, 'N', 'T', 'C'}, optional
+            Type of system to solve:
+
+            ========  =========
+            trans     system
+            ========  =========
+            0 or 'N'  a x  = b
+            1 or 'T'  a^T x = b
+            2 or 'C'  a^H x = b
+            ========  =========
+        unit_diagonal : bool, optional
+            If True, diagonal elements of `a` are assumed to be 1 and
+            will not be referenced.
+        overwrite_b : bool, optional
+            Allow overwriting data in `b` (may enhance performance)
+        check_finite : bool, optional
+            Whether to check that the input matrices contain only finite numbers.
+            Disabling may give a performance gain, but may result in problems
+            (crashes, non-termination) if the inputs do contain infinities or NaNs.
+
+        Returns
+        -------
+        x : (M,) or (M, N) ndarray
+            Solution to the system `a x = b`.  Shape of return matches `b`.
+
+        Raises
+        ------
+        Exception
+            If `a` is singular
+
+        Notes
+        -----
+        .. versionadded:: 0.9.0
+
+        """
+
+        if check_finite:
+            a1, b1 = map(np.asarray_chkfinite, (a, b))
+        else:
+            a1, b1 = map(np.asarray, (a, b))
+        if len(a1.shape) != 2 or a1.shape[0] != a1.shape[1]:
+            raise ValueError("expected square matrix")
+        if a1.shape[0] != b1.shape[0]:
+            raise ValueError("incompatible dimensions")
+        overwrite_b = False  # overwrite_b or _datacopied(b1, b)
+        if debug:
+            print("solve:overwrite_b=", overwrite_b)
+        trans = {"N": 0, "T": 1, "C": 2}.get(trans, trans)
+        (trtrs,) = get_lapack_funcs(("trtrs",), (a1, b1))
+        x, info = trtrs(
+            a1,
+            b1,
+            overwrite_b=overwrite_b,
+            lower=lower,
+            trans=trans,
+            unitdiag=unit_diagonal,
+        )
+
+        if info == 0:
+            return x
+        if info > 0:
+            raise Exception(
+                "singular matrix: resolution failed at diagonal %s" % (info - 1)
+            )
+        raise ValueError("illegal value in %d-th argument of internal trtrs" % -info)
+
+    def transpose(self, a):
+        dim = a.shape
+        if len(dim) != 2:
+            raise ValueError("Expected a matrix")
+        aT = np.zeros(dim[::-1], dtype=a.dtype)
+        for ii, aa in enumerate(a):
+            aT[:, ii] = a[ii]
+        return aT
+
+
+class EmpiricalInterpolation(LinAlg):
+    """
+    Class for building an empirical interpolant
+    """
+
+    def __init__(self):
+        LinAlg.__init__(self)
+
+    def malloc_ei(self, Nbasis, Nquads, Nmodes=1, dtype="complex"):
+        self.indices = malloc("int", Nbasis)
+        self.invV = malloc(dtype, Nbasis, Nbasis)
+        self.R = malloc(dtype, Nbasis, Nquads)
+        self.B = malloc(dtype, Nbasis, Nquads)
+        pass
+
+    def coefficient(self, invV, e, indices):
+        return np.dot(invV.T, e[indices])
+
+    def residual(self, e, c, R):
+        """Difference between a basis function 'e' and its empirical interpolation"""
+        return e - np.dot(c, R)
+
+    def next_invV_col(self, R, indices, check_finite=False):
+        b = np.zeros(len(indices), dtype=R.dtype)
+        b[-1] = 1.0
+        return self.solve_triangular(
+            R[:, indices], b, lower=False, check_finite=check_finite
+        )
+        # return solve_triangular(
+        #    R[:,indices], b, lower=False, check_finite=check_finite)
+
+    def eim_interpolant(self, invV, R):
+        """The empirical interpolation matrix 'B'"""
+        return np.dot(invV, R)
+
+    def eim_interpolate(self, h, indices, B):
+        """Empirically interpolate a function"""
+        dim = np.shape(h)
+        if len(dim) == 1:
+            return np.dot(h[indices], B)
+        elif len(dim) > 1:
+            return np.array([np.dot(h[ii][indices], B) for ii in range(dim[0])])
+
+
+class StandardEIM(EmpiricalInterpolation):
+    def __init__(self, Nbasis, Nquads, Nmodes=1, dtype="complex"):
+        EmpiricalInterpolation.__init__(self)
+
+        # Allocate memory for numpy arrays
+        self.malloc_ei(Nbasis, Nquads, Nmodes=Nmodes, dtype=dtype)
+        self.modes = Nmodes
+        self.quads = Nquads
+
+    def seed(self, e):
+        """Seed the algorithm"""
+        self.indices[0] = np.argmax(np.abs(e))
+        self.R[0] = e
+        self.invV[:1, :1] = self.next_invV_col(self.R[:1], self.indices[:1])
+        pass
+
+    def iter(self, step, e):
+        """One iteration in the empirical interpolation greedy algorithm"""
+
+        ctr = step + 1
+
+        # Compute interpolant residual
+        c = self.coefficient(self.invV[:ctr, :ctr], e, self.indices[:ctr])
+        r = self.residual(e, c, self.R[:ctr])
+
+        # Update
+        self.indices[ctr] = np.argmax(np.abs(r))
+        self.R[ctr] = r
+        self.invV[:, ctr][: ctr + 1] = self.next_invV_col(
+            self.R[: ctr + 1], self.indices[: ctr + 1]
+        )
+        pass
+
+    def make(self, basis, verbose=True, timerQ=False):
+        """Make an empirical interpolant using the standard greedy algorithm"""
+
+        # Seed the greedy algorithm
+        self.seed(basis[0])
+
+        # EIM algorithm with reduced complexity for inverting the van der Monde matrix
+        if verbose:
+            print("\nIter", "\t", "Indices")
+        if timerQ:
+            t0 = time.time()
+        dim = len(basis)
+        for ctr in range(dim - 1):
+            if verbose:
+                print(ctr + 1, "\t", self.indices[ctr])
+
+            # Single iteration
+            self.iter(ctr, basis[ctr + 1])
+
+        if timerQ:
+            print("\nElapsed time =", time.time() - t0)
+
+        # Compute interpolant matrix 'B'
+        # if flag == 1:
+        #     self.size = ctr
+        # else:
+        #     self.size = ctr+2
+        # self.trim(ctr+1)
+        self.trim(ctr + 2)
+        self.make_interpolant()
+        self.size = len(self.indices)
+        pass
+
+    def make_interpolant(self):
+        self.B = self.eim_interpolant(self.invV, self.R)
+        pass
+
+    def interpolate(self, h):
+        return self.eim_interpolate(h, self.indices, self.B)
+
+    def trim(self, num):
+        """Trim zeros from remaining entries"""
+        if num > 0:
+            self.indices = self.indices[:num]
+            self.R = self.R[:num]
+            self.invV = self.invV[:num, :num]
+            self.B = self.B[:num, :]
+        pass
 
 
 class EmpiricalInterpolant(object):
@@ -49,9 +292,7 @@ class EmpiricalInterpolant(object):
 
         ######################################################
         # TODO: move the code for these calls into this class
-        eim = algorithms.StandardEIM(
-            reduced_basis.matrix_shape[0], reduced_basis.matrix_shape[1]
-        )
+        eim = StandardEIM(reduced_basis.matrix_shape[0], reduced_basis.matrix_shape[1])
         eim.make(reduced_basis.matrix)
         ######################################################
 
--- python/rombus/tests/test_rombus.py
+++
@@ -1,6 +0,0 @@
-import rombus._core.integrals as integrals
-
-
-def test_rate_to_num():
-    x = integrals.rate_to_num(0, 10, 50)
-    assert x is not None
