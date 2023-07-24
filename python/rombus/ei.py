94a642e532c2786cb1263df2acb0902abbf16d8a
Gregory Poole <gbpoole@gmail.com>
2023-07-24 12:52:19 +1000

    Debugging CLI testing error.

--- python/rombus/ei.py
+++ python/rombus/ei.py
@@ -279,16 +279,11 @@ class EmpiricalInterpolant(object):
         domain = reduced_basis.model.domain
         self.nodes = domain[eim.indices]
 
-        log.comment(f"nodes: {type(self.nodes)} --- {self.nodes.shape}")
-        log.comment(f"nodes: {self.nodes}")
-        log.comment(f"eim:B: {type(eim.B)} --- {eim.B.shape}")
         nodes_sorted, B_matrix_sorted = zip(
             *sorted(zip(self.nodes, eim.B), key=lambda x: x[0])
         )
         self.nodes = np.asarray(nodes_sorted)
         self.B_matrix = np.asarray(B_matrix_sorted)
-        log.comment(f"nodes: {type(self.nodes)} --- {self.nodes.shape}")
-        log.comment(f"B_mtx: {type(self.B_matrix)} --- {self.B_matrix.shape}")
 
         return self
 
